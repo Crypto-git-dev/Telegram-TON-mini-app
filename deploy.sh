7b1e72416e07467815738048a62c5d59f563b3ed
Crypto Dev <experiencedtopdev@gmail.com>
2024-08-06 13:30:50 +0500

    erfe

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,10 @@ PROJECT_DIR="/root/TONixHUB_frontend"
 BUILD_DIR="/var/www/html"
 
 # Убедитесь, что вы находитесь в правильной директории проекта
-cd "$PROJECT_DIR" || { echo "Директория $PROJECT_DIR не найдена"; exit 1; }
+if ! cd "$PROJECT_DIR"; then
+    echo "Директория $PROJECT_DIR не найдена"
+    exit 1
+fi
 
 # Проверка наличия nvm и установка последней версии Node.js
 if command -v nvm &> /dev/null; then
@@ -37,15 +40,28 @@ fi
 # Синхронизация с удаленным репозиторием и переключение на нужную ветку
 echo "Переключение на ветку $BRANCH"
 git fetch origin
-git checkout $BRANCH || { echo "Не удалось переключиться на ветку $BRANCH"; exit 1; }
-git pull origin $BRANCH || { echo "Не удалось обновить ветку $BRANCH"; exit 1; }
+if ! git checkout "$BRANCH"; then
+    echo "Не удалось переключиться на ветку $BRANCH"
+    exit 1
+fi
+
+if ! git pull origin "$BRANCH"; then
+    echo "Не удалось обновить ветку $BRANCH"
+    exit 1
+fi
 
 # Установка зависимостей
 echo "Установка зависимостей"
 if [ -f package-lock.json ]; then
-    npm install || { echo "Не удалось установить npm-зависимости"; exit 1; }
+    if ! npm install; then
+        echo "Не удалось установить npm-зависимости"
+        exit 1
+    fi
 elif [ -f yarn.lock ]; then
-    yarn install || { echo "Не удалось установить yarn-зависимости"; exit 1; }
+    if ! yarn install; then
+        echo "Не удалось установить yarn-зависимости"
+        exit 1
+    fi
 else
     echo "Файл package-lock.json или yarn.lock не найден. Проверьте наличие файла и повторите попытку."
     exit 1
@@ -53,20 +69,32 @@ fi
 
 # Сборка проекта
 echo "Сборка проекта"
-npm run build || { echo "Сборка проекта завершилась неудачей"; exit 1; }
+if ! npm run build; then
+    echo "Сборка проекта завершилась неудачей"
+    exit 1
+fi
 
 # Создание директории для сборки, если она не существует
 if [ ! -d "$BUILD_DIR" ]; then
     echo "Создание директории $BUILD_DIR"
-    mkdir -p "$BUILD_DIR" || { echo "Не удалось создать директорию $BUILD_DIR"; exit 1; }
+    if ! mkdir -p "$BUILD_DIR"; then
+        echo "Не удалось создать директорию $BUILD_DIR"
+        exit 1
+    fi
 fi
 
 # Очистка директории для сборки
 echo "Очистка директории $BUILD_DIR"
-rm -rf "$BUILD_DIR"/* || { echo "Не удалось очистить директорию $BUILD_DIR"; exit 1; }
+if ! rm -rf "$BUILD_DIR"/*; then
+    echo "Не удалось очистить директорию $BUILD_DIR"
+    exit 1
+fi
 
 # Копирование новой сборки
 echo "Копирование новой сборки в $BUILD_DIR"
-cp -r "$PROJECT_DIR/build/"* "$BUILD_DIR" || { echo "Не удалось скопировать файлы в $BUILD_DIR"; exit 1; }
+if ! cp -r "$PROJECT_DIR/build/"* "$BUILD_DIR"; then
+    echo "Не удалось скопировать файлы в $BUILD_DIR"
+    exit 1
+fi
 
 echo "Проект успешно задеплоен в $BUILD_DIR"
